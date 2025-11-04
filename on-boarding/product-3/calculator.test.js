8d1cdfced2de72df9ba1735ff48f7ae48526ea4e
eyob worku <eyobworku62@gmail.com>
2025-11-04 21:13:56 +0300

    Enhance add function to support string concatenation

--- on-boarding/product-3/calculator.test.js
+++ on-boarding/product-3/calculator.test.js
@@ -4,4 +4,9 @@ describe("add", () => {
   test("should add two positive numbers correctly", () => {
     expect(add(2, 3)).toBe(5);
   });
+
+  // New test for the new feature
+  test("should concatenate two strings", () => {
+    expect(add("hello", " world")).toBe("hello world");
+  });
 });
